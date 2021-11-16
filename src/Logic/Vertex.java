ff423200eb14dc6be881bfefdac1efde49ac5ced
pankaj1707k <pankaj.1707k@gmail.com>
2021-11-16 19:13:58 +0530

    Vertex: Initial attributes and get/set methods

--- src/Logic/Vertex.java
+++ src/Logic/Vertex.java
@@ -1,4 +1,32 @@
 package Logic;
 
 public class Vertex {
+    private double x;
+    private double y;
+
+    Vertex(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    } 
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setCoordinates(double x, double y) {
+        setX(x);
+        setY(y);
+    }
 }
