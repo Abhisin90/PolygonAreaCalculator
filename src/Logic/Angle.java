040689b31c68bda6b4f70e8e109c7c837f220f93
pankaj1707k <pankaj.1707k@gmail.com>
2021-11-28 23:35:20 +0530

    Modified calculateAngle() implementation
    Fixed bug that showed outer angle in case of concave polygon

--- src/Logic/Angle.java
+++ src/Logic/Angle.java
@@ -3,18 +3,12 @@ package Logic;
 public class Angle {
     private Line line1;
     private Line line2;
-    private double oppositeDiagonalLength;
     private double angle;
 
     public Angle(Line l1, Line l2) {
         this.line1 = l1;
         this.line2 = l2;
         this.angle = 90.0;
-        int x1 = l1.getEndPoint1().getX();
-        int y1 = l1.getEndPoint1().getY();
-        int x2 = l2.getEndPoint2().getX();
-        int y2 = l2.getEndPoint2().getY();
-        this.oppositeDiagonalLength = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
     }
 
     public double getAngle() {
@@ -26,17 +20,21 @@ public class Angle {
     }
 
     public double calculateAngle() {
-        double l1 = line1.getLength();
-        double l2 = line2.getLength();
         int x1 = line1.getEndPoint1().getX();
         int y1 = line1.getEndPoint1().getY();
         int x2 = line2.getEndPoint2().getX();
         int y2 = line2.getEndPoint2().getY();
-        this.oppositeDiagonalLength = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-        double dl = oppositeDiagonalLength;
-        double angle = Math.acos((l1 * l1 + l2 * l2 - dl * dl) / (2 * l1 * l2));
-        angle = Math.round(Math.toDegrees(angle) * 100) / 100.0;
+        int cvx = line1.getEndPoint2().getX();
+        int cvy = line1.getEndPoint2().getY();
+        double a1 = Math.atan2(x1 - cvx, y1 - cvy);
+        a1 = Math.round(Math.toDegrees(a1) * 100) / 100.0;
+        double a2 = Math.atan2(x2 - cvx, y2 - cvy);
+        a2 = Math.round(Math.toDegrees(a2) * 100) / 100.0;
+        double a = a2 - a1;
+        if (a < 0) {
+            a += 360;
+        }
 
-        return angle;
+        return a;
     }
 }
